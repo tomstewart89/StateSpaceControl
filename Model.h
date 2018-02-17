995cb4d5dde716a7368878d68b26e7338c3bf55b
Tom Stewart <tomstewart3107@gmail.com>
2018-02-17 23:47:15 +0900

    fixed up documentation, added simulation class

--- Model.h
+++ Model.h
@@ -1,7 +1,23 @@
 using namespace BLA;
 
-template<int X, int U, int Y> struct Model
+/*
+ * This file defines a Model class to represent a state space model. Model has three template parameters:
+ *
+ * X := the number of states
+ * U := the number of control inputs (inputs to the model, outputs from the controller)
+ * Y := the number of outputs (sensor measurements of the system which are fed back to the controller)
+ *
+ * You can define any state space model by declaring a Model<X,U,Y> and filling out the system matrics, A, B, C & D appropriately.
+ * I've also added a couple of subclasses of Model to represent model that are commonly used in state space control, those being
+ * DC motor control for servos and Cart Pole for inverted pendulum problems. I'm planning on expanding on this list but in the meantime
+ * feel free to PR if you come up with one of your own that might be widely useful!
+ */
+template<int X, int U, int Y=X> struct Model
 {
+    const static int states = X;
+    const static int inputs = U;
+    const static int outputs = Y;
+
     Matrix<X,X> A;
     Matrix<X,U> B;
     Matrix<Y,X> C;
@@ -31,8 +47,6 @@ struct CartPoleModel : public Model<4,1,2>
   {
     float c = (I * (M + m) + M * m * l * l);
 
-    // Now assuming a state of: [cart_position cart_velocity stick_angle stick_rate]^T we can define the inverted pendulum's dynamics as follows:
-
     // Define the system matrix
     A << 0, 1, 0, 0,
          0, -(I + m * l * l) * b / c, m * m * g * l * l / c, 0,
@@ -48,5 +62,49 @@ struct CartPoleModel : public Model<4,1,2>
     // Define the output matrix
     C << 1, 0, 0, 0,
          0, 0, 1, 0;
+
+    // Define the direct transmission matrix
+    D = Zeros<2,1>();
+  }
+};
+
+
+/*
+ * This model describes the position of a DC motor based on a voltage input. The actual modeling for this came from http://ctms.engin.umich.edu/CTMS/index.php?example=InvertedPendulum&section=SystemModeling
+ * which defines the state as:
+ *
+ * x := [cart_position, cart_velocity, stick_angle, stick_angular_rate]^T
+ *
+ * And it is assumed that 1: the controller manipulates the voltage supplied to the motor and 2: the motor position, velocity and current are all directly measureable meaning that the system has full state feedback
+ *
+ * To parameterise the system the following physical measurements are required:
+ *
+ * J := rotor inertia                       (kg.m^2)
+ * b = viscous friction coefficient         (N.m.s)
+ * K = back emf constant
+ * R = armature resistance                  (ohm)
+ * L = armature inductance                  (H)
+ */
+struct MotorPositionModel : public Model<3,1>
+{
+  MotorPositionModel(float J, float b, float k, float R, float L)
+  {
+    // Define the system matrix
+    A << 0, 1, 0,
+         0, -b / J, k / J,
+         0, -k / L, -R / L;
+
+    // Define the input matrix
+    B << 0,
+         0,
+         1 / L;
+
+    // Define the output matrix
+    C << 1, 0, 0,
+         0, 1, 0,
+         0, 0, 1;
+
+    // Define the direct transmission matrix
+    D = Zeros<3,1>();
   }  
 };
--- StateSpaceControl.h
+++ StateSpaceControl.h
@@ -7,17 +7,20 @@
 
 using namespace BLA;
 
+
 template<bool ENABLE, int X, int Y = 1> struct EnableMatrixIf
 {
   typedef Matrix<X, Y> T;
 };
+
 
 template<int X, int Y> struct EnableMatrixIf<false, X, Y>
 {
   typedef Zeros<X, Y> T;
 };
 
-template <int X, int U, int Y=X, bool EnableReferenceTracking=true, bool EnableEstimation=false, bool EnableIntegralControl=false>
+
+template <int X, int U, int Y=X, bool EnableEstimation=false, bool EnableIntegralControl=false, bool EnableReferenceTracking=true>
 class StateSpaceController
 {
     typename EnableMatrixIf<EnableEstimation, X, X>::T ALC;
@@ -25,7 +28,7 @@ class StateSpaceController
 
   public:
     // System model
-    Model<X, U, Y> model;
+    const Model<X, U, Y> &model;
 
     // Control variables
     Matrix<X> x_hat; // state estimate
@@ -34,11 +37,10 @@ class StateSpaceController
     typename EnableMatrixIf<EnableIntegralControl, U>::T w_hat; // estimate of a disturbance / error in the system model (used by the integral controller)
 
     // Control Gains
-    Matrix<U, X> K; // Regulator Gain
+    Matrix<U, X> K; // regulator Gain
     typename EnableMatrixIf<EnableEstimation, X, Y>::T L; // estimator gain
     typename EnableMatrixIf<EnableIntegralControl, U, Y>::T I; // integral control gain
 
-    StateSpaceController() : x_hat(Zeros<X>()), u(Zeros<U>()), r(Zeros<Y>()), w_hat(Zeros<U>()) { }
     StateSpaceController(const Model<X, U, Y>& _model) : model(_model), x_hat(Zeros<X>()), u(Zeros<U>()), r(Zeros<Y>()), w_hat(Zeros<U>()) { }
 
     void initialise()
@@ -48,7 +50,16 @@ class StateSpaceController
         auto sys = (model.A || model.B) && (model.C || model.D);
 
         // Find an inverse for the aggregated matrix
-        Matrix <X+U,X+Y > sysInv = ~sys * (sys * ~sys).Inverse();
+        Matrix <X+U,X+Y> sysInv;
+
+        // case 1: more outputs than inputs - find the left inverse
+        if(model.inputs < model.outputs) {
+            sysInv = (~sys * sys).Inverse() * ~sys;
+        }
+        // case 2: more than, or the same number of outputs as inputs - find the right inverse
+        else {
+            sysInv = ~sys * (sys * ~sys).Inverse();
+        }
 
         // Split it up and multiply it with K to find NBar
         N_bar = K * sysInv.Submatrix(Slice<0, X>(), Slice <X,X+Y> ()) + sysInv.Submatrix(Slice <X,X+U > (), Slice <X,X+Y> ());
@@ -60,7 +71,7 @@ class StateSpaceController
       }
     }
 
-    void update(const Matrix<Y> &y, float dt)
+    void update(const Matrix<Y> &y, const float dt=0.0f)
     {
       // If estimation is enabled, update the state estimate
       if (EnableEstimation) {
@@ -86,6 +97,23 @@ class StateSpaceController
         u += w_hat;
       }
     }
+};
+
+
+template<int X, int U, int Y=X> class Simulation
+{
+public:
+  Matrix<X> x;
+
+  const Model<X,U,Y>& model;
+
+  Simulation(const Model<X,U,Y>& _model) : model(_model), x(Zeros<X>()) { }
+
+  Matrix<Y> step(const Matrix<U>& u, const float dt)
+  {
+      x += (model.A * x + model.B * u) * dt;
+      return model.C * x;
+  }
 };
 
 #endif // STATE_SPACE_CONTROL_H
